8c8e72642b7beb3e5c706e78a0eaeb77733025c8
Patrick5432 <112853910+Patrick5432@users.noreply.github.com>
2024-03-03 22:32:59 +0500

    Завершение числовой угадайки

---
+++ guess_number.py
@@ -0,0 +1,85 @@
+import random
+
+#Генерируется рандомное число
+
+def randomNumber():
+    hiddenNumber = random.randint(1, 10)
+    return hiddenNumber
+
+hideNumber = randomNumber()
+
+#Изменяет имя
+
+def changeName():
+    namePerson = input("Введите ваше имя: ")
+    return namePerson
+
+#Начинает игру(действует один раз)
+
+def startGame():
+    print("Игра начинается")
+    hNumber = hideNumber
+    namePerson = changeName()
+    namePerson = namePerson.replace(" ", "")
+    if len(namePerson) != 0:
+        guessNumber(hNumber, namePerson)
+    else :
+        print("Вы не ввели имя")
+        print("Игра завершается")
+
+#Продолжает игру(можно использовать бесконечно)
+
+def contGame():
+    check = input("Продолжите игру? ")
+    
+    if check.lower() == "yes" or check.lower() == "да":
+        checkName = input("Желаете изменить имя? ")
+        if checkName.lower() == "yes" or checkName.lower() == "да":
+            name = changeName()
+            hNumber = randomNumber()
+            guessNumber(hNumber, name)
+        else:
+            return False
+    elif check.lower() == "no" or check.lower() == "нет":
+        print("Игра завершена")
+        return True
+        
+
+#Сама игра
+
+def guessNumber(hideNumber, namePerson):
+    finishRound = False
+    attemptCounter = 0
+
+    while finishRound == False:
+        print(hideNumber)
+        number = int(input("Введите число: "))
+
+        if hideNumber < number:
+            if number - hideNumber <= 2:
+                print("Близко")
+            else:
+                print("Меньше")
+
+        elif hideNumber > number:
+            if hideNumber - number <= 2:
+                print("Близко")
+            else:
+                print("Больше")
+
+        if hideNumber == number:
+            print("Вы угадали число")
+            print(namePerson, "ваше число", hideNumber)
+            finishRound = contGame()
+            if finishRound == False:
+                guessNumber(randomNumber(), namePerson) 
+        else:
+            attemptCounter += 1
+            if attemptCounter == 3:
+                endGame = input("Желаете закончить игру? ")
+                if endGame.lower() == "yes" or endGame.lower() == "да":
+                    print("Игра завершена")
+                    finishRound = True
+                attemptCounter = 0
+
+startGame()
